23065783ae47752ff6299fb497b95d28ccde4c9b
DamianBlock <damianfrt25@gmail.com>
2023-12-01 14:02:25 -0300

    Probando metodo suma y resta de enteros, negativos y decimales.

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,8 +1,8 @@
 import dominio.Calculadora;
 import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.Assert.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculadoraTest {
 
